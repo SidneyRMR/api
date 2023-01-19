46e73e2156aceffc59a548804271e24961c97619
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2023-01-19 20:16:21 -0300

    adição do myportsql no arquivo db.js

--- db.js
+++ db.js
@@ -4,14 +4,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const db = mysql.createConnection({
-    // host: 'localhost',
-    // user: "igreja-api",
-    // password: "123456",
-    // database: "bd_igreja_vendas_v1"
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 })
 
 //configurar rotas para usuarios
