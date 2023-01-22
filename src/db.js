dc0fd759b1c820c4395d364b1b4bf1bed23577bf
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2023-01-22 05:23:24 -0300

    teste

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
