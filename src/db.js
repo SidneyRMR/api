4a41244195adf1ffada59dc31d75291101e87fb4
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2023-01-22 19:27:39 -0300

    variavel mysqlport no db adicionada

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@ export const pool = mysql.createPool({
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
-    // port: process.env.MYSQLPORT
+    port: process.env.MYSQLPORT
 })
 
 //configurar rotas para usuarios
