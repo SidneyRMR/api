a2cba27d5ee404193eca37d14e6497835d2c7289
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2023-01-24 22:31:12 -0300

    cors teste

--- src/db.js
+++ src/db.js
@@ -9,7 +9,7 @@ export const pool = mysql.createPool({
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
     port: process.env.MYSQLPORT,
-    // waitForConnections: true,
+    waitForConnections: true,
     // connectionLimit: 10,
     // queueLimit: 0
 })
